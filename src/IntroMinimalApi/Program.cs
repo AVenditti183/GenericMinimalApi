a369c5ae72e9efed9337bbbdda3aadfa72051650
Diego Zunino <diego.zunino@hotmail.it>
2022-05-22 22:19:23 +0200

    Fix

--- src/IntroMinimalApi/Program.cs
+++ src/IntroMinimalApi/Program.cs
@@ -43,7 +43,7 @@ app.MapPost("/blexiners", (IService<Blexiner> service, Blexiner blexiner) =>
     return Results.CreatedAtRoute("GetBlexiner", new { newBlexiner.Id }, newBlexiner);
 })
 .WithName("PostBlexiner")
-.Produces(StatusCodes.Status201Created, typeof(Guid))
+.Produces(StatusCodes.Status201Created, typeof(Blexiner))
 .ProducesValidationProblem();
 
 app.MapPut("blexiners/{id:guid}", (IService<Blexiner> service, Guid id, Blexiner blexiner) =>
