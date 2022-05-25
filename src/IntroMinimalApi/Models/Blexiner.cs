2e15e767c2314f40c67534defdd2826bfa6ce8ee
Diego Zunino <diego.zunino@hotmail.it>
2022-05-25 10:33:01 +0200

    Data annotation

--- src/IntroMinimalApi/Models/Blexiner.cs
+++ src/IntroMinimalApi/Models/Blexiner.cs
@@ -1,8 +1,14 @@
-﻿namespace IntroMinimalApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroMinimalApi.Models;
 public class Blexiner
 {
     public Guid Id { get; set; }
+    [Required]
+    [MaxLength(30)]
     public string FirstName { get; set; }
+    [Required]
+    [MaxLength(30)]
     public string LastName { get; set; }
     public string JobTitle { get; set; }
 }
